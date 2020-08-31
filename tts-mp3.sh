ddc261aff0021e1edc09928099ad0693d3d003c6
Pascal Jordin <jordin@sipgate.de>
2020-08-31 10:15:55 +0200

    ✨ multiple folder with final textes

--- tts-mp3.sh
+++ tts-mp3.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 shopt -s nullglob
 
-MP3_FOLDER=./mp3
+text_to_speech(){
+  MP3_FOLDER=./mp3
 
-for FILE in ./text/*.json
-do
+  FILE=$1
+  SUBFOLDER_WITH_FILE=${FILE#./*/}
+  FOLDER=${SUBFOLDER_WITH_FILE%/*.json}
   FILENAME=${FILE##*/}
   NAME=${FILENAME%.*}
   LANGCODE=`cat $FILE | jq -r ".voice.languageCode | tostring"`
-  VOICE=`cat $FILE | jq -r ".voice.ssmlGender | tostring"`
 
-  echo "Processing ${FILENAME} file with language ${LANGCODE} and ${VOICE}..."
+  echo "Processing ${FILENAME} file with language ${LANGCODE}..."
   
   curl -X POST \
   -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
@@ -18,5 +19,25 @@ do
   -d @$FILE \
   https://texttospeech.googleapis.com/v1/text:synthesize | \
   jq -r '.audioContent | tostring' | \
-  base64 --decode > "${MP3_FOLDER}/${NAME}.mp3"
-done
+  base64 --decode > "${MP3_FOLDER}/${FOLDER}/${NAME}.mp3"
+  echo ""
+}
+
+batch_text_to_speech(){
+  
+  echo "Processing files in text folder \"${1}\""
+  echo ""
+  echo "#########################################################################"
+  for FILE in ./text/$1/*.json
+  do
+    text_to_speech "${FILE}"
+  done
+  
+  echo "#########################################################################"
+  echo ""
+}
+
+batch_text_to_speech "de"
+batch_text_to_speech "en"
+
+# text_to_speech ./text/en/step-keyword-submitted.json
